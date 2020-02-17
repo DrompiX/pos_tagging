63d9ecbf41198e32381a6f805b5ca920557dd7db
Dima T <mrlocky98@gmail.com>
2020-02-17 15:18:41 +0300

    Simple NN structure was added

--- tagger_train.py
+++ tagger_train.py
@@ -11,6 +11,32 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
+class POSTagger(nn.Module):
+
+    def __init__(self, hidden_dim, target_size, w_embedding_dim,
+                 w_vocab_size, c_embedding_dim, c_vocab_size):
+        super(POSTagger, self).__init__()
+        # TODO: Add more params
+        self.lstm = nn.LSTM(hidden_size=hidden_dim, bidirectional=True)
+        self.cnn = nn.Sequential(
+            # TODO: define structure
+        )
+        self.word_embeddings = nn.Embedding(w_vocab_size, w_embedding_dim)
+        self.hidden2tag = nn.Linear(hidden_dim, target_size)
+
+    def forward(self, sentence):
+        w_embeddings = self.word_embeddings(sentence)
+        c_embeddings = self.cnn(sentence)
+
+        full_embedding = torch.Tensor()  # TODO: concat word and character embeddings
+
+        lstm_out, _ = self.lstm(full_embedding)
+        tag_space = self.hidden2tag(lstm_out)
+        # TODO: Do we need this? Cross-entropy uses softmax under the hood (IINM)
+        # tag_scores = F.log_softmax(tag space)
+        return tag_space
+
+
 def read_train_data(data_path: str) -> list:
     training_data = []
     with open(data_path, 'r') as fd:
@@ -18,6 +44,7 @@ def read_train_data(data_path: str) -> list:
         for sentence in data:
             words2tokens = []
             for tagged_word in sentence.split():
+                # TODO: Create tuples for each element in sequence of / ???
                 word, tag = tuple(tagged_word.rsplit('/', 1))
                 words2tokens.append((word.lower(), tag))
             
@@ -47,8 +74,12 @@ def train_model(train_file, model_file):
     EMBEDDING_DIM, HIDDEN_DIM = 7, 7
     training_data = read_train_data(train_file)
     word_to_ix, tag_to_ix = get_word_tag_mappings(training_data)
-    
-    print(word_to_ix)
+
+    pos_model = POSTagger()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(pos_model.parameters(), lr=0.001)
+
+
     print('Finished...')
 
 
