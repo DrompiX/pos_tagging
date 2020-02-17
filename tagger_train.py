f0213fe9720c2af0c42d728ade25d7a6c27ed3a0
Dima T <mrlocky98@gmail.com>
2020-02-17 13:18:12 +0300

    Reading of train data was implemented

--- tagger_train.py
+++ tagger_train.py
@@ -11,10 +11,44 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
-def train_model(train_file, model_file):
-    # write your code here. You can add functions as well.
-    # use torch library to save model parameters, hyperparameters, etc. to model_file
+def read_train_data(data_path: str) -> list:
+    training_data = []
+    with open(data_path, 'r') as fd:
+        data = fd.readlines()
+        for sentence in data:
+            words2tokens = []
+            for tagged_word in sentence.split():
+                word, tag = tuple(tagged_word.rsplit('/', 1))
+                words2tokens.append((word.lower(), tag))
+            
+            training_data.append(tuple(map(list, zip(*words2tokens))))
+    
+    return training_data
 
+
+def get_word_tag_mappings(training_data: list) -> (dict, dict):
+    word_to_ix = {}
+    tag_to_ix = {}
+    for sent, tags in training_data:
+        for word in sent:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+        
+        for tag in tags:
+            if tag not in tag_to_ix:
+                tag_to_ix[tag] = len(tag_to_ix)
+
+    return word_to_ix, tag_to_ix
+
+
+def train_model(train_file, model_file):
+    # Write your code here. You can add functions as well. Use torch 
+    # library to save model parameters, hyperparameters, etc. to model_file
+    EMBEDDING_DIM, HIDDEN_DIM = 7, 7
+    training_data = read_train_data(train_file)
+    word_to_ix, tag_to_ix = get_word_tag_mappings(training_data)
+    
+    print(word_to_ix)
     print('Finished...')
 
 
